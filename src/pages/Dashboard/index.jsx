1fde4844d31651867fb8c40aca8fb64f25024c0f
Vilson Padilha <vilson.neto57@gmail.com>
2021-08-15 21:57:16 -0300

    feat: Adicionado funcionalidade que define a porcentagem do componente ProgressBar; - Foi feito uma estilização básica da page Dashboard.

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,35 @@
+import { useContext, useEffect, useState } from "react";
 import { ContainerHabits } from "../../components/ContainerHabits";
 import Header from "../../components/header";
+import ProgressBar from "../../components/ProgressBar";
+import { HabitsContext } from "../../providers/Habits";
+import { Container } from "./styles";
 
 export const Dashboard = () => {
+  const { habits } = useContext(HabitsContext);
+  const [progressHabits, setProgressHabits] = useState(0);
+
+  useEffect(() => {
+    const totalHabits = habits.length;
+    const achivedHabits = [...habits].filter(
+      (habit) => habit.achieved === true
+    );
+    const progress = (100 / totalHabits) * achivedHabits.length;
+
+    setProgressHabits(progress);
+  }, [habits]);
+
   return (
-    <div>
+    <>
       <Header />
-      <ContainerHabits />
-    </div>
+      <Container>
+        <div className="progress-container">
+          <ProgressBar percent={progressHabits} />
+        </div>
+        <h2>Habits</h2>
+        <ContainerHabits />
+        <h2>My Groups</h2>
+      </Container>
+    </>
   );
 };
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .progress-container {
+    min-width: 313px;
+    width: 80%;
+    margin: 18px auto 24px;
+  }
+  h2 {
+    margin: 13px 0;
+    text-indent: 23px;
+    color: var(--black);
+    font-size: 24px;
+    font-weight: 700;
+  }
+`;
