1d833e739170edffa8a3a873a2c282167289ec97
joaogarbini <joaogarbini@hotmail.com>
2021-08-17 20:11:23 -0300

    estilização groups e dashboard

--- src/components/ListAllGroups/styles.jsx
+++ src/components/ListAllGroups/styles.jsx
@@ -1,48 +1,76 @@
 import styled from "styled-components";
+
+export const Title = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  margin: 10px;
+  margin-top: 30px;
+`;
+
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-left: 20%;
+  margin-right: 20%;
   justify-content: center;
   align-items: center;
-  margin-top: 30px;
+  margin-top: 40px;
 `;
 export const Lista = styled.div`
   font-size: 22px;
   padding: 5px;
-  width: 400px;
-  height: 60px;
-
+  width: 250px;
+  height: 350px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  background-color: #050a20;
   border: 1px solid;
   border-color: transparent;
   border-radius: 5px;
-  background-color: var(--white);
-  box-shadow: 0 1px 3px 0 rgb(26 24 29 / 62%), 0 1px 2px 0 rgb(26 24 29 / 24%);
-  margin: 5px;
+  border: inset;
+  border-color: var(--red);
+  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+
+  margin: 15px;
   display: flex;
   align-items: center;
+  .logo {
+    width: 200px;
+    margin-top: 20px;
+  }
+
+  .group-information {
+    margin-top: 20px;
+  }
   p {
-    max-width: 30ch;
+    max-width: 25ch;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
+    font-size: 12px;
+    font-weight: bold;
+    margin: 10px;
+    margin-top: 20px;
+    color: var(--white);
   }
   .check-icon {
     margin: 10px;
     border: solid 1.5px;
     border-radius: 5px;
-    color: var(--green);
-    height: 30px;
-    width: 30px;
+    color: var(--red);
+    height: 45px;
+    width: 45px;
     cursor: pointer;
-    box-shadow: 0 1.5px 0 #002000;
+    box-shadow: 0 1.5px 0 #000000;
     background-color: white;
   }
 
   .check-icon:active {
-    position: relative;
     top: 1.5px;
     box-shadow: none;
-    color: var(--red);
-    background-color: var(--green);
+    color: var(--white);
+    background-color: var(--red);
   }
 `;
