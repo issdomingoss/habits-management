b13aa968e1d894a0ea4b81f982f9219da1546a23
ItaloSilva <iss.domingoss@gmail.com>
2021-08-19 15:31:06 -0300

    fix: fixing bug about link to contact US

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -20,25 +20,25 @@ const Contact = () => {
                 </ContainerCard>
             </LinkTo>
 
-            <LinkTo target='_blank' href='https://www.linkedin.com/in/guilherme-armesto-job-a91b72204'>
+            <LinkTo target='_blank' href='https://www.linkedin.com/in/italo-silva-536b6111b/'>
                 <ContainerCard>
                     <h2>Italo Domingos</h2>
                     <GrLinkedin />
                 </ContainerCard>
             </LinkTo>
-            <LinkTo target='_blank' href='https://www.linkedin.com/in/guilherme-armesto-job-a91b72204'>
+            <LinkTo target='_blank' href='https://www.linkedin.com/in/jo%C3%A3o-garbini-294a393a/'>
                 <ContainerCard>
                     <h2>João Garbini</h2>
                     <GrLinkedin />
                 </ContainerCard>
             </LinkTo>
-            <LinkTo target='_blank' href='https://www.linkedin.com/in/guilherme-armesto-job-a91b72204'>
+            <LinkTo target='_blank' href='linkedin.com/in/miqueias-carvalho-dos-santos'>
                 <ContainerCard>
                     <h2>Miqueias Carvalho</h2>
                     <GrLinkedin />
                 </ContainerCard>
             </LinkTo>
-            <LinkTo target='_blank' href='https://www.linkedin.com/in/guilherme-armesto-job-a91b72204'>
+            <LinkTo target='_blank' href='https://www.linkedin.com/in/vilson-neto-40539b17b/'>
                 <ContainerCard>
                     <h2>Vilson Neto</h2>
                     <GrLinkedin />
