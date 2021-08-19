d863edb1be8fba5ceee2e58a88a0e9666de2541f
Miqueias <miqs_carvalho@hotmail.com>
2021-08-19 18:30:05 -0300

    feat: mostrando que o usuário está seguindo um grupo ao clicar no botão

--- src/components/ListAllGroups/index.jsx
+++ src/components/ListAllGroups/index.jsx
@@ -4,9 +4,14 @@ import { useContext, useEffect, useState } from "react";
 import { GroupsContext } from "../../providers/Groups";
 import { Lista, Container, Title } from "./styles";
 import Logo from "../../assets/logo_Speak_Tracks.PNG";
+import jwt_decode from "jwt-decode";
+
 const ListAllGroups = () => {
   const { allGroups, subscribeGroup, myGroups } = useContext(GroupsContext);
   const [att, setAtt] = useState(0);
+  const [userOnToken] = useState(
+    jwt_decode(JSON.parse(localStorage.getItem("token")))
+  );
 
   useEffect(() => {
     setAtt(att + 1);
@@ -19,12 +24,20 @@ const ListAllGroups = () => {
         {allGroups.map((item) => (
           <Lista key={item.id}>
             <img className="logo" src={Logo} alt="logo" />
+
             <div className="group-information">
               <p className="group_Name">{item.name}</p>
               <p>Description: {item.description}</p>
               <p className="followers">
                 Followers: {item.users_on_group.length}
               </p>
+
+              {item.users_on_group.map(
+                (user) =>
+                  user.id === userOnToken.user_id && (
+                    <p className="following"> Following</p>
+                  )
+              )}
             </div>
 
             <AiOutlineUsergroupAdd
--- src/components/ListAllGroups/styles.jsx
+++ src/components/ListAllGroups/styles.jsx
@@ -36,8 +36,9 @@ export const Lista = styled.div`
   margin: 15px;
   display: flex;
   align-items: center;
+
   .logo {
-    width: 200px;
+    width: 150px;
     margin-top: 20px;
   }
 
@@ -60,8 +61,12 @@ export const Lista = styled.div`
     font-size: 18px;
     text-align: center;
   }
-  .followers {
+  .followers,
+  .following {
     text-align: center;
+  }
+  .following {
+    color: var(--green);
   }
 
   .check-icon {
